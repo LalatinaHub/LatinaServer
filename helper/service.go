bbf7b585130880bbb0b5b5689f469332a537e5ee
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2023-11-15 17:29:59 +0700

    Ignore restart error

--- helper/service.go
+++ helper/service.go
@@ -6,13 +6,13 @@ import (
 )
 
 func ReloadService(names ...string) {
-	defer CatchError(true)
-
 	for _, name := range names {
 		fmt.Println("Restarting", name, "...")
 		_, err := exec.Command("systemctl", "restart", name).Output()
 		if err != nil {
-			panic(err)
+			fmt.Println("Failed restarting", name)
+			fmt.Println("[", name, "]", err.Error())
+			continue
 		}
 
 		fmt.Println(name, "successfully restarted !")
