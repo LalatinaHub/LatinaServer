40092ce40e09e86b81f227a215b1ba418aa7f870
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2024-01-20 07:05:07 +0700

    Exclude dummy account

--- db/db.go
+++ db/db.go
@@ -68,7 +68,7 @@ func GetPremiumList() map[string][]PremiumTable {
 		rows        = []PremiumTable{}
 	)
 
-	if err := Connect().DB.From("premium").Select("*").Execute(&rows); err != nil {
+	if err := Connect().DB.From("premium").Select("*").Neq("type", "dummy").Execute(&rows); err != nil {
 		panic(err)
 	}
 
