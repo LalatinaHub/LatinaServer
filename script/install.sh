fedc261d635581440487bed927a2d6513ee5b2ce
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2024-11-24 17:45:50 +0700

    Use pre-built binaries

--- script/install.sh
+++ script/install.sh
@@ -2,8 +2,13 @@
 
 DIR=$(dirname "$0")
 PROJECT=$DIR/..
+GITHUB_TOKEN="${GITHUB_TOKEN}"
+ARCHIVE_NAME="latinaserver.tar.gz"
 
-go build -tags with_grpc,with_clash_api,with_v2ray_api,with_utls,with_reality_server,with_gvisor,with_quic -o $PROJECT/latinaserver $PROJECT/cmd/latinaserver/main.go
+LATINASERVER_DOWNLOAD_URL=$(curl -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/LalatinaHub/LatinaServer/releases | jq ".[0].assets.[0].browser_download_url")
+
+wget -O $PROJECT/$ARCHIVE_NAME $LATINASERVER_DOWNLOAD_URL
+tar -xzf $PROJECT/$ARCHIVE_NAME
 
 sudo mkdir -p /usr/local/etc/latinaserver
 sudo cp $PROJECT/resources/openresty/nginx.conf /usr/local/etc/latinaserver/
@@ -18,3 +23,6 @@ sudo cp $PROJECT/latinaserver /usr/local/bin/
 sudo systemctl daemon-reload
 
 sudo systemctl start latinaserver
+
+sudo rm -rf $PROJECT/$ARCHIVE_NAME
+sudo rm -rf $PROJECT/latinaserver
