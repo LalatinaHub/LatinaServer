354ee8ea0b83b794c88ec1e4343c8a45d7f5d1dd
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2025-05-02 18:13:40 +0700

    Update domain.go

--- constant/domain.go
+++ constant/domain.go
@@ -1,6 +1,6 @@
 package constant
 
 var (
-	IP_RESOLVER_DOMAIN = "myip.shylook.workers.dev"
-	IP_RESOLVER_PATH   = "/"
+	IP_RESOLVER_DOMAIN = "cloudflare-ip.html.zone"
+	IP_RESOLVER_PATH   = "/geo"
 )
